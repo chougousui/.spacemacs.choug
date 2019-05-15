a845667a13631dbb82b2a10b195b946f70317259
chougousui <chougousui@163.com>
2019-05-15 10:39:59 +0900

    为常用的helm-projectile-ag绑定快捷键

--- choug/keybindings.el
+++ choug/keybindings.el
@@ -1,3 +1,4 @@
 (global-set-key (kbd "C-=") 'er/expand-region)
 (global-set-key (kbd "C-s") 'swiper)
 (global-set-key (kbd "C-S-s") 'choug/swiper-dwim)
+(global-set-key (kbd "C-M-s") 'helm-projectile-ag)
