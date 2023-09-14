0279d83fb9507fbf4e66faa2a8d046ee6ec8aa4d
choug <chougousui@gmail.com>
2023-09-15 01:39:10 +0900

    添加两个常用函数

--- layers/choug/funcs.el
+++ layers/choug/funcs.el
@@ -25,3 +25,17 @@ With argument ARG, do this that many times."
   (if (region-active-p)
       (call-interactively #'delete-region)
     (choug/backward-delete-word arg)))
+
+(defun choug/minify ()
+  "从光标所在位置,至匹配的括号处,将选定的内容合并为一行"
+  (interactive)
+  (let ((start (point)))
+    (save-excursion
+      (forward-list)
+      (delete-indentation nil start (point)))))
+
+;; 自定义一个lsp相关函数,让lsp能移除未使用的imports
+;; 但在启动lsp-mode之后才开始定义,否则函数找不到
+;; (add-hook 'lsp-mode-hook '(lambda () (lsp-make-interactive-code-action remove-unused-imports "source.removeUnusedImports")))
+(with-eval-after-load 'lsp-mode
+  (lsp-make-interactive-code-action remove-unused-imports "source.removeUnusedImports"))
