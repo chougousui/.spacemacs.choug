7e1ca8e5518b861ba704e0fa0980df6e736744b4
choug <chougousui@gmail.com>
2024-05-30 11:40:39 +0900

    设置emacs内部的自定义双拼方案,以备不时之需

--- layers/choug/config.el
+++ layers/choug/config.el
@@ -117,3 +117,58 @@
           (lambda ()
             (when (eq python-formatter 'ruff)
               (ruff-format-on-save-mode t))))
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 添加一个自定义的双拼输入法方案(小鹤workman)
+;; 使用时设置pyim-default-scheme 'flypy-workman即可
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defvar flypy-workman-scheme
+   '(flypy-workman
+   :document "参考小鹤双拼韵母在workman键盘布局上的位置,修改而来"
+   :class shuangpin
+   :first-chars "abcdefghijklmnpqrstuvwxyz"
+   :rest-chars "abcdefghijklmnopqrstuvwxyz"
+   :prefer-triggers ("o")
+   :cregexp-support-p t
+   :keymaps
+   (("a" "a" "a")
+    ("b" "b" "ue" "ve")
+    ("c" "c" "ao")
+    ("d" "d" "iang" "uang")
+    ("e" "sh" "e" "e")
+    ("f" "f" "ing" "uai")
+    ("g" "g" "ang")
+    ("h" "h" "ai")
+    ("i" "ch" "i")
+    ("j" "j" "un")
+    ("k" "k" "iao")
+    ("l" "l" "ian")
+    ("m" "m" "in")
+    ("n" "n" "en")
+    ("o" "o" "uo" "o")                  ; o重复了两次,不知道为什么这样写
+    ("p" "p" "ie")
+    ("q" "q" "iu")
+    ("r" "r" "uan")
+    ("s" "s" "iong" "ong")
+    ("t" "t" "an")
+    ("u" "zh" "u")
+    ("v" "v" "ui")
+    ("w" "w" "ei")
+    ("x" "x" "ia" "ua")
+    ("y" "y" "eng")
+    ("z" "z" "ou")
+    ("aa" "a")
+    ("aj" "an")
+    ("ad" "ai")
+    ("ac" "ao")
+    ("ah" "ang")
+    ("ee" "e")
+    ("er" "er")
+    ("eg" "eng")
+    ("ag" "ng")
+    ("ao" "o")
+    ("au" "ou"))))
+
+(with-eval-after-load 'pyim
+  (pyim-scheme-add flypy-workman-scheme)
+  )
